29c3e7df68d80c8daf8ee2e9ef9ca2e8110ae650
Ricardo <ry.yrupailla@gmail.com>
2019-07-16 17:39:58 -0500

    Separate reducers into files

--- client/src/redux/reducer.jsx
+++ client/src/redux/reducer.jsx
@@ -1,42 +1,5 @@
 import { combineReducers } from "redux";
-import initialState from "./initialState"
-
-function userReducer(state = initialState.user, action = {}) {
-  switch (action.type) {
-
-    case "LOGIN_REQUEST": {
-      return {
-        ...state,
-        loading: true
-      };
-    }
-    case "LOGIN_SUCCESS": {
-      return {
-        ...state,
-        currentUser: action.payload,
-        loading: false
-      };
-    }
-    case "LOGOUT": {
-      return {
-        ...state,
-        currentUser: {},
-        loading: false,
-        error: null
-      };
-    }
-    case "LOGIN_ERROR": {
-      return {
-        ...state,
-        error: action.payload,
-        loading: false
-      };
-    }
-    default: {
-      return state;
-    }
-  }
-}
+import userReducer from "./reducers/userReducer";
 
 const reducer = combineReducers({
   user: userReducer
