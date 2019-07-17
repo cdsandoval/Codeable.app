97a2c69b11012c20acf5bdac09e3cae83c969ad2
byteconde <iamdeyvi@gmail.com>
2019-07-17 11:52:56 -0500

    Add destroy all to avoid repetitions

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -5,6 +5,7 @@ User.create(name: 'Mayra Navarro', email: 'mnavarro@able.co', password: '123456'
 User.create(name: 'carlos Sandoval', email: 'csandoval@able.co', password: '123456') 
 
 # Modules
+Modulo.destroy_all
 Modulo.create(name: 'Ruby Fundamentals' ,description: 'Ruby Fundamentals is an inmersive module for beginners')
 Modulo.create(name: 'Ruby on Rails' ,description: 'The most used framework for Ruby developers. We will learn how to create an entire backend project from scratch')
 Modulo.create(name: 'React' ,description: 'React is one of the most demanded libraries to build interfaces. We will learn how to create an entire frontend project from scratch')
@@ -12,6 +13,7 @@ Modulo.create(name: 'Final Project' ,description: 'At the end of the module all 
 
 
 # Lessons
+Lesson.destroy_all
 Lesson.create(title:'Install Fest, CLI & Basic Programming', modulo_id: 1)
 Lesson.create(title:'Git, Methods & Classes', modulo_id: 1)
 Lesson.create(title:'Database Overview', modulo_id: 2)
@@ -23,6 +25,7 @@ Lesson.create(title:'Brainstorming', modulo_id: 4)
 
 
 # Mini
+MiniAssignment.destroy_all
 MiniAssignment.create(title:'Build a simple calculator',content:
   '
   ## Build a basic calculator using Ruby
@@ -39,7 +42,7 @@ MiniAssignment.create(title:'Build a BMI calculator',content:
   ', lesson_id: 2 )
 
 # Sublesson
-
+Sublesson.destroy_all
 Sublesson.create(title:'CLI Commands',content:'## Learn some basic commands like: ls, cd, mkdir, touch, rm -rf, mv, cat, history, grep, find, chmod',tag_id:1,lesson_id:1,section:'main')
 Sublesson.create(title:'How to use commands with CLI',content:'## Play with your files using some commands',tag_id:2,lesson_id:1,section:'main')
 Sublesson.create(title:'Create your first repository',content:'## Create your first repository on Github and apply some commands',tag_id:3,lesson_id:1,section:'main')
@@ -47,6 +50,7 @@ Sublesson.create(title:'Check extra exercises',content:'## Make your first pull 
 Sublesson.create(title:'Read additional material about CLI',content:'## How to be a rockstart within CLI',tag_id:5,lesson_id:1,section:'extra')
 
 # Tag
+Tag.destroy_all
 Tag.create(name: 'OVERVIEW', color: 'green')
 Tag.create(name: 'WORKSHOP', color: 'blue')
 Tag.create(name: 'M-ASSIGN', color: 'orange')
