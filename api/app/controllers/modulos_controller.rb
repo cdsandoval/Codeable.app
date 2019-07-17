b3d3f8c0c5461dbdccae1a20e6a8587b5d58667c
byteconde <iamdeyvi@gmail.com>
2019-07-17 11:53:51 -0500

    Add modulos response with API

--- api/app/controllers/modulos_controller.rb
+++ api/app/controllers/modulos_controller.rb
@@ -1,2 +1,26 @@
 class ModulosController < ApplicationController
+  before_action :set_modulo, only: [:show]
+
+    def index
+      @modulos = Modulo.all
+      render json: {
+        status: 'SUCCESS',
+        message: 'Loaded Modulos',
+        data: @modulos
+      } , status: :ok
+    end
+  
+    def show
+      render json: {
+        status: 'SUCCESS',
+        message: 'Loaded Modulo',
+        data: @modulo
+      }
+    end
+  
+    private
+    def set_modulo
+      @modulo = Modulo.find(params[:id])
+    end
+
 end
--- api/config/routes.rb
+++ api/config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   # sessions routes
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+  resources :modulos do
+    resources :lessons
+  end
+
 end
